9d9135785d5a7a52f916cfb34d3e261012fde612
Md. Parvej <62025097+Parvej23@users.noreply.github.com>
2022-08-12 11:46:09 +0600

    Add files via upload

---
+++ Take input adjacency list ( weighted ).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mx=1e5+123;
+vector<pair<int, int>> adj[mx];
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    for(int i=1;i<m;i++)
+    {
+        int u, v, w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Adj nodes of node "<<i<<" : ";
+        for(auto u: adj[i]){
+            cout<<"Node : "<<u.first<<" "<<"Weight : "<<u.second<<endl;
+        }
+        cout<<endl;
+    }
+}
